77af3381544b141da46e7244c8a69c3f2a40dccf
Michael Miller <41575314+Miller202@users.noreply.github.com>
2019-11-01 10:30:51 -0300

    Update somatorios.pl

--- somatorios.pl
+++ somatorios.pl
@@ -1,5 +1,7 @@
 somatorio(11, _).
-somatorio(I, Som) :- Soma is (1/(2^I)) + Som, In is I + 1, write(Soma), nl, somatorio(In, Soma).
+somatorio(I, S) :- Soma is (1/(2**I)) + S, In is I + 1, 
+write(Soma), nl, somatorio(In, Soma).
 
 somatorio1(11, _).
-somatorio1(I, Som) :- Soma is (((-1)^I)*2) + Som, In is I + 1, write(Soma), nl, somatorio1(In, Soma).
+somatorio1(I, S) :- Soma is (((-1)**I)*2) + S, In is I + 1, 
+write(Soma), nl, somatorio1(In, Soma).
